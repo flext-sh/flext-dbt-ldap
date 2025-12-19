6e828e964b6d12046e18962272db977cb28fc950
Marlon Costa <marlon.costa@datacosmos.com.br>
2025-12-19 15:48:33 -0300

    updates

--- src/flext_dbt_ldap/simple_api.py
+++ src/flext_dbt_ldap/simple_api.py
@@ -16,10 +16,10 @@ from flext_core import (
     r,
 )
 
-from flext_dbt_ldap.config import FlextDbtLdapSettings
 from flext_dbt_ldap.dbt_client import FlextDbtLdapClient
 from flext_dbt_ldap.dbt_services import FlextDbtLdapService
 from flext_dbt_ldap.models import FlextDbtLdapModels
+from flext_dbt_ldap.settings import FlextDbtLdapSettings
 from flext_dbt_ldap.typings import t
 from flext_dbt_ldap.utilities import FlextDbtLdapUtilities
 
--- target/compiled/flext_dbt_ldap/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-"""FLEXT - Enterprise Data Integration Platform.
-
-Copyright (c) 2025 FLEXT Team. All rights reserved.
-SPDX-License-Identifier: MIT
-
-"""
-
-
-# Package initializer
