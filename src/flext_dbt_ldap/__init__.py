85abacf04378b612bc0159e0d7e795d33156cacc
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-06 02:46:40 -0300

    chore(codegen): inline lazy __init__.py, eliminate _exports.py sidecar
    Regenerated by 'flext_infra codegen init --apply': PEP 562 lazy imports are
    now inlined directly into each package __init__.py; the separate _exports.py
    lazy-map sidecar is removed (superseded by the inline map).

--- src/flext_dbt_ldap/__init__.py
+++ src/flext_dbt_ldap/__init__.py
@@ -5,7 +5,11 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from flext_core.lazy import install_lazy_exports
+from flext_core.lazy import (
+    build_lazy_import_map,
+    install_lazy_exports,
+    merge_lazy_imports,
+)
 from flext_dbt_ldap.__version__ import (
     __author__,
     __author_email__,
@@ -16,85 +20,85 @@ from flext_dbt_ldap.__version__ import (
     __version__,
     __version_info__,
 )
-from flext_dbt_ldap._exports import FLEXT_DBT_LDAP_LAZY_IMPORTS
 
 if TYPE_CHECKING:
-    from flext_dbt_ldap.api import FlextDbtLdap as FlextDbtLdap, dbt_ldap as dbt_ldap
-    from flext_dbt_ldap.base import (
-        FlextDbtLdapServiceBase as FlextDbtLdapServiceBase,
-        s as s,
-    )
-    from flext_dbt_ldap.constants import (
-        FlextDbtLdapConstants as FlextDbtLdapConstants,
-        c as c,
-    )
-    from flext_dbt_ldap.models import FlextDbtLdapModels as FlextDbtLdapModels, m as m
-    from flext_dbt_ldap.protocols import (
-        FlextDbtLdapProtocols as FlextDbtLdapProtocols,
-        p as p,
-    )
-    from flext_dbt_ldap.services.client import (
-        FlextDbtLdapClientMixin as FlextDbtLdapClientMixin,
-    )
-    from flext_dbt_ldap.services.sync import (
-        FlextDbtLdapSyncMixin as FlextDbtLdapSyncMixin,
-    )
-    from flext_dbt_ldap.settings import FlextDbtLdapSettings as FlextDbtLdapSettings
-    from flext_dbt_ldap.typings import FlextDbtLdapTypes as FlextDbtLdapTypes, t as t
-    from flext_dbt_ldap.utilities import (
-        FlextDbtLdapUtilities as FlextDbtLdapUtilities,
-        u as u,
-    )
-    from flext_ldap import d as d, e as e, h as h, r as r, x as x
-
-
-_LAZY_IMPORTS = FLEXT_DBT_LDAP_LAZY_IMPORTS
-
-
-_EAGER_EXPORTS = (
-    __author__,
-    __author_email__,
-    __description__,
-    __license__,
-    __title__,
-    __url__,
-    __version__,
-    __version_info__,
+    from flext_dbt_ldap.api import FlextDbtLdap
+    from flext_dbt_ldap.base import FlextDbtLdapServiceBase, s
+    from flext_dbt_ldap.constants import FlextDbtLdapConstants, c
+    from flext_dbt_ldap.models import FlextDbtLdapModels, m
+    from flext_dbt_ldap.protocols import FlextDbtLdapProtocols, p
+    from flext_dbt_ldap.services.client import FlextDbtLdapClientMixin
+    from flext_dbt_ldap.services.sync import FlextDbtLdapSyncMixin
+    from flext_dbt_ldap.settings import FlextDbtLdapSettings
+    from flext_dbt_ldap.typings import FlextDbtLdapTypes, t
+    from flext_dbt_ldap.utilities import FlextDbtLdapUtilities, u
+    from flext_ldap import d, e, h, r, x
+_LAZY_IMPORTS = merge_lazy_imports(
+    (".services",),
+    build_lazy_import_map(
+        {
+            ".api": (
+                "FlextDbtLdap",
+                "dbt_ldap",
+            ),
+            ".base": (
+                "FlextDbtLdapServiceBase",
+                "s",
+            ),
+            ".constants": (
+                "FlextDbtLdapConstants",
+                "c",
+            ),
+            ".models": (
+                "FlextDbtLdapModels",
+                "m",
+            ),
+            ".protocols": (
+                "FlextDbtLdapProtocols",
+                "p",
+            ),
+            ".services.client": ("FlextDbtLdapClientMixin",),
+            ".services.sync": ("FlextDbtLdapSyncMixin",),
+            ".settings": ("FlextDbtLdapSettings",),
+            ".typings": (
+                "FlextDbtLdapTypes",
+                "t",
+            ),
+            ".utilities": (
+                "FlextDbtLdapUtilities",
+                "u",
+            ),
+            "flext_ldap": (
+                "d",
+                "e",
+                "h",
+                "r",
+                "x",
+            ),
+        },
+    ),
+    exclude_names=(
+        "cleanup_submodule_namespace",
+        "install_lazy_exports",
+        "lazy_getattr",
+        "logger",
+        "merge_lazy_imports",
+        "output",
+        "output_reporting",
+        "pytest_addoption",
+        "pytest_collect_file",
+        "pytest_collection_modifyitems",
+        "pytest_configure",
+        "pytest_runtest_setup",
+        "pytest_runtest_teardown",
+        "pytest_sessionfinish",
+        "pytest_sessionstart",
+        "pytest_terminal_summary",
+        "pytest_warning_recorded",
+    ),
+    module_name=__name__,
 )
 
-
-_PUBLIC_EXPORTS: tuple[str, ...] = (
-    "FlextDbtLdap",
-    "FlextDbtLdapClientMixin",
-    "FlextDbtLdapConstants",
-    "FlextDbtLdapModels",
-    "FlextDbtLdapProtocols",
-    "FlextDbtLdapServiceBase",
-    "FlextDbtLdapSettings",
-    "FlextDbtLdapSyncMixin",
-    "FlextDbtLdapTypes",
-    "FlextDbtLdapUtilities",
-    "dbt_ldap",
-    "__author__",
-    "__author_email__",
-    "__description__",
-    "__license__",
-    "__title__",
-    "__url__",
-    "__version__",
-    "__version_info__",
-    "c",
-    "d",
-    "e",
-    "h",
-    "m",
-    "p",
-    "r",
-    "s",
-    "t",
-    "u",
-    "x",
-)
 
 __all__: tuple[str, ...] = (
     "FlextDbtLdap",
@@ -134,5 +138,5 @@ install_lazy_exports(
     __name__,
     globals(),
     _LAZY_IMPORTS,
-    public_exports=_PUBLIC_EXPORTS,
+    public_exports=__all__,
 )
