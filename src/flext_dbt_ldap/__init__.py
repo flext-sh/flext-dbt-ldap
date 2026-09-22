91a5b88fa79094bbbd24436bc853960221b46156
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-22 16:22:48 -0300

    fixes

--- src/flext_dbt_ldap/__init__.py
+++ src/flext_dbt_ldap/__init__.py
@@ -20,25 +20,21 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_cli import cli
-    from flext_ldap import ldap
-    from flext_ldif import ldif
-    from flext_meltano import main, meltano
-
-    from flext_core import core, d, e, h, lazy_attribute, r, x
+    from flext_ldap import d, e, h, r, x
 
     from . import services
+    from .__version__ import FlextDbtLdapVersion
     from ._config import FlextDbtLdapConfig, config
     from ._settings import FlextDbtLdapSettings, settings
     from .api import FlextDbtLdap, dbt_ldap
     from .base import FlextDbtLdapServiceBase, FlextDbtLdapServiceBase as s
-    from .constants import FlextDbtLdapConstants, c
-    from .models import FlextDbtLdapModels, m
-    from .protocols import FlextDbtLdapProtocols, p
+    from .constants import FlextDbtLdapConstants, FlextDbtLdapConstants as c
+    from .models import FlextDbtLdapModels, FlextDbtLdapModels as m
+    from .protocols import FlextDbtLdapProtocols, FlextDbtLdapProtocols as p
     from .services.client import FlextDbtLdapClientMixin
     from .services.sync import FlextDbtLdapSyncMixin
-    from .typings import FlextDbtLdapTypes, t
-    from .utilities import FlextDbtLdapUtilities, u
+    from .typings import FlextDbtLdapTypes, FlextDbtLdapTypes as t
+    from .utilities import FlextDbtLdapUtilities, FlextDbtLdapUtilities as u
 __all__: tuple[str, ...] = (
     "FlextDbtLdap",
     "FlextDbtLdapClientMixin",
@@ -51,6 +47,7 @@ __all__: tuple[str, ...] = (
     "FlextDbtLdapSyncMixin",
     "FlextDbtLdapTypes",
     "FlextDbtLdapUtilities",
+    "FlextDbtLdapVersion",
     "__author__",
     "__author_email__",
     "__description__",
@@ -60,19 +57,12 @@ __all__: tuple[str, ...] = (
     "__version__",
     "__version_info__",
     "c",
-    "cli",
     "config",
-    "core",
     "d",
     "dbt_ldap",
     "e",
     "h",
-    "lazy_attribute",
-    "ldap",
-    "ldif",
     "m",
-    "main",
-    "meltano",
     "p",
     "r",
     "s",
@@ -86,6 +76,7 @@ __all__: tuple[str, ...] = (
 _LAZY_IMPORTS = MappingProxyType(
     build_lazy_import_map(
         MappingProxyType({
+            ".__version__": ("FlextDbtLdapVersion",),
             "._config": ("FlextDbtLdapConfig", "config"),
             "._settings": ("FlextDbtLdapSettings", "settings"),
             ".api": ("FlextDbtLdap", "dbt_ldap"),
@@ -98,11 +89,7 @@ _LAZY_IMPORTS = MappingProxyType(
             ".services.sync": ("FlextDbtLdapSyncMixin",),
             ".typings": ("FlextDbtLdapTypes", "t"),
             ".utilities": ("FlextDbtLdapUtilities", "u"),
-            "flext_cli": ("cli",),
-            "flext_core": ("core", "d", "e", "h", "lazy_attribute", "r", "x"),
-            "flext_ldap": ("ldap",),
-            "flext_ldif": ("ldif",),
-            "flext_meltano": ("main", "meltano"),
+            "flext_ldap": ("d", "e", "h", "r", "x"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
