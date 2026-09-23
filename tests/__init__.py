7be6cde6f8455fcda8f51b25c13670e03661847c
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-23 14:19:37 -0300

    fixes

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,27 +9,30 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_cli import cli
-    from flext_ldap import ldap
-    from flext_ldif import ldif
+    from flext_ldap import adapters, ldap, ldif
     from flext_meltano import meltano
     from flext_tests import (
-        active_rules,
         api,
+        cli,
         config,
-        discover_repository_root,
+        core,
+        d,
+        e,
+        h,
         install_local_packages,
+        lazy_attribute,
         load_infra_report,
+        r,
+        services,
         settings,
-        split_csv,
         td,
         tf,
         tk,
         tm,
         tv,
+        x,
     )
 
-    from flext_core import core, d, e, h, lazy_attribute, r, x
     from flext_dbt_ldap import dbt_ldap, main
 
     from . import e2e, unit
@@ -50,7 +53,7 @@ __all__: tuple[str, ...] = (
     "TestsFlextDbtLdapSettings",
     "TestsFlextDbtLdapTypes",
     "TestsFlextDbtLdapUtilities",
-    "active_rules",
+    "adapters",
     "api",
     "c",
     "cli",
@@ -58,7 +61,6 @@ __all__: tuple[str, ...] = (
     "core",
     "d",
     "dbt_ldap",
-    "discover_repository_root",
     "e",
     "e2e",
     "h",
@@ -73,8 +75,8 @@ __all__: tuple[str, ...] = (
     "p",
     "r",
     "s",
+    "services",
     "settings",
-    "split_csv",
     "t",
     "td",
     "tf",
@@ -98,26 +100,29 @@ _LAZY_IMPORTS = MappingProxyType(
             ".typings": ("TestsFlextDbtLdapTypes", "t"),
             ".unit": ("unit",),
             ".utilities": ("TestsFlextDbtLdapUtilities", "u"),
-            "flext_cli": ("cli",),
-            "flext_core": ("core", "d", "e", "h", "lazy_attribute", "r", "x"),
             "flext_dbt_ldap": ("dbt_ldap", "main"),
-            "flext_ldap": ("ldap",),
-            "flext_ldif": ("ldif",),
+            "flext_ldap": ("adapters", "ldap", "ldif"),
             "flext_meltano": ("meltano",),
             "flext_tests": (
-                "active_rules",
                 "api",
+                "cli",
                 "config",
-                "discover_repository_root",
+                "core",
+                "d",
+                "e",
+                "h",
                 "install_local_packages",
+                "lazy_attribute",
                 "load_infra_report",
+                "r",
+                "services",
                 "settings",
-                "split_csv",
                 "td",
                 "tf",
                 "tk",
                 "tm",
                 "tv",
+                "x",
             ),
         }),
         alias_groups=MappingProxyType({}),
